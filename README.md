2c547cd936de67731bd4b0a3fa0075f343c3c678
DarrelA <darrelaiscoding@gmail.com>
2022-03-05 23:06:35 +0800

    Sending request to country layer api to check country name using country code.

--- README.md
+++ README.md
@@ -6,6 +6,14 @@
 ## Installation
 
 1. Install Live Server extension on VS Code.
+2. Create config.js in root async-hangman folder
+   ```sh
+   touch config.js
+   ```
+3. Insert <i>countrylayer</i> API key. <i>This is totally <span style="color: red"><b>UNSAFE</b></i></span> for production as you can retrieve the key front client side.
+   ```js
+   const API_KEY_COUNTRYLAYER = 'someAPIkey'; // https://countrylayer.com/
+   ```
 
 ## Notes
 
--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// HTTP (Hypertext Transfer Protocol)
-// Request - What do we want to do
-// Response - What was actually done
-
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
 const game1 = new Hangman('Car Parts', 2);
@@ -29,3 +25,20 @@ request.addEventListener('readystatechange', (e) => {
 // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/open
 request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=9');
 request.send();
+
+const countryRequest = new XMLHttpRequest();
+const countryCode = 'MX';
+
+countryRequest.addEventListener('readystatechange', (e) => {
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText);
+    const country = data.find((country) => country.alpha2Code === countryCode);
+    console.log(country.name);
+  } else if (e.target.readyState === 4) console.log('Something went wrong!');
+});
+
+countryRequest.open(
+  'GET',
+  `http://api.countrylayer.com/v2/all?access_key=${API_KEY_COUNTRYLAYER}`
+);
+countryRequest.send();

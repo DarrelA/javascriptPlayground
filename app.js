a16191d723c8f89b1dcdc37f294674c46cd802c6
DarrelA <darrelaiscoding@gmail.com>
2022-03-23 23:12:58 +0800

    Refactor and implement a reusable debouce().

--- app.js
+++ app.js
@@ -10,9 +10,14 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector('input');
 
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) clearTimeout(timeoutId);
-  timeoutId = setTimeout(() => fetchData(event.target.value), 500);
+// Deboucing an input: Waiting for some time to pass after the last event to actually do something.
+const debounce = (func, delay = 700) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(null, args), delay);
+  };
 };
-input.addEventListener('input', onInput);
+
+const onInput = (event) => fetchData(event.target.value);
+input.addEventListener('input', debounce(onInput));

0c5a9b3c9a624360db724bf90917b4c0b405c1c3
DarrelA <darrelaiscoding@gmail.com>
2022-03-23 18:09:07 +0800

    SVG circle layout.

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   // Emit an event stating that the timer has started.
   onStart() {
     console.log('Timer started.');
   },
+
   onTick() {
-    console.log('Ticking');
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset = currentOffset - 50;
   },
+
   onComplete() {
     console.log('Beep Beep Beep!');
   },

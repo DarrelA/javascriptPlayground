27dc9ec41f7226cb8da30e2efff46c06ab0d2eff
DarrelA <darrelaiscoding@gmail.com>
2022-03-26 13:46:47 +0800

    Replace home build bodyParser with express.urlencoded({ extended: true }).

--- app.js
+++ app.js
@@ -24,22 +24,7 @@ app.get('/', (req, res) =>
 `)
 );
 
-const bodyParser = (req, res, next) => {
-  if (req.method === 'POST') {
-    req.on('data', (data) => {
-      const parsed = data.toString('utf8').split('&');
-      const formData = {};
-      for (let pair of parsed) {
-        const [key, value] = pair.split('=');
-        formData[key] = value;
-      }
-      req.body = formData;
-      next();
-    });
-  } else next();
-};
-
-app.post('/', bodyParser, (req, res) => {
+app.post('/', express.urlencoded({ extended: true }), (req, res) => {
   console.log(req.body);
   res.send('Account created!');
 });

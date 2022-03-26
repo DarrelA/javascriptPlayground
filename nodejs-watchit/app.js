26525de55f98bb34a2c96aedd8d6fa4878a8dc28
DarrelA <darrelaiscoding@gmail.com>
2022-03-26 11:14:53 +0800

    child_process.spawn() with Node.js library to forward information to process then to terminal.

--- nodejs-watchit/app.js
+++ nodejs-watchit/app.js
@@ -3,19 +3,33 @@
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
 const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
 const colors = require('colors');
 
 program
   .version('0.0.1')
   .argument('[filename]', 'Name of a file to execute')
-  .action((args) => console.log(args));
+  .action(async ({ filename }) => {
+    const name = filename || app.js;
+
+    try {
+      await fs.promises.access(name);
+    } catch (error) {
+      throw new Error(`Could not find the file ${[name]}.`.brightRed);
+    }
+
+    // https://nodejs.org/dist/latest-v16.x/docs/api/child_process.html#optionsstdio
+    const start = debounce(
+      () => spawn('node', [name], { stdio: 'inherit' }),
+      100
+    );
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
 
 program.parse(process.argv);
-
-const start = debounce(() => console.log("STARTING USER'S PROGRAM".green), 100);
-
-chokidar
-  .watch('.')
-  .on('add', start)
-  .on('change', () => console.log('FILE CHANGED'.brightYellow))
-  .on('unlink', () => console.log('FILE UNLINKED'.brightRed));

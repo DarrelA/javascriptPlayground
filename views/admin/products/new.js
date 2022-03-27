70752f1c8b43edb09c1069126e4de2ee094a2e64
DarrelA <darrelaiscoding@gmail.com>
2022-03-27 19:12:26 +0800

    Bug fix: Rearrange the order of multer middleware & body-parser middleware.

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -7,8 +7,10 @@ module.exports = ({ errors }) => {
     content: /* html*/ 
       `<form method="POST" enctype="multipart/form-data">
          <input placeholder="title" name="title" />
+         ${getError(errors, 'title')}
          <input placeholder="price" name="price" />
          <input type="file" name="image" />
+         ${getError(errors, 'price')}
          <button>Submit</button>
        </form>`,
   });
